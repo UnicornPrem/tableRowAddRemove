740c3fa9bc69618d87f182fb2a0d910d3ad9af79
Prem Bikram Limbu <moranprem@rocketmail.com>
2015-07-31 19:51:44 +0530

    use jquery files for .before
    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/jquery-ui.min.js"></script>

--- addrow.js
+++ addrow.js
@@ -1,25 +1,19 @@
 var product_row = 0;
 
 function addProduct() {
-	html  = '<tbody id="product-row' + product_row + '">';
-	html += '  <tr>'; 
-  html += '    <td class="left"><input type="text" name="product[' + product_row + '][name]" value=""/></td>';		
-  html += '    <td class="right"><input type="text" name="product[' + product_row + '][quantity]" value="" size="2" /></td>';
-  html += '    <td class="right"><input type="text" name="product[' + product_row + '][listing_priority]" value="" size="2" /></td>';
-	html += '    <td class="right"><input type="text" name="product[' + product_row + '][price]" value="" /></td>';
-  html += '    <td class="left"><input type="text" name="product[' + product_row + '][code]" value="" /></td>';
-	html += '    <td class="left"><input type="text" name="product[' + product_row + '][added_date]" value="" class="added_date" /></td>';
-	html += '    <td class="left"><a onclick="$(\'#product-row' + product_row + '\').remove();" class="button">Remove</a></td>';
-	html += '  </tr>';	
-  html += '</tbody>';
+html  = '<tbody id="product-row' + product_row + '">';
+html += '  <tr>'; 
+html += '    <td class="left"><input type="text" name="product[' + product_row + '][name]" value=""/></td>';		
+html += '    <td class="right"><input type="text" name="product[' + product_row + '][quantity]" value="" size="2" /></td>';
+html += '    <td class="right"><input type="text" name="product[' + product_row + '][listing_priority]" value="" size="2" /></td>';
+html += '    <td class="right"><input type="text" name="product[' + product_row + '][price]" value="" /></td>';
+html += '    <td class="left"><input type="text" name="product[' + product_row + '][code]" value="" /></td>';
+html += '    <td class="left"><input type="text" name="product[' + product_row + '][added_by]" value=""/></td>';
+html += '    <td class="left"><a onclick="$(\'#product-row' + product_row + '\').remove();" class="button">Remove</a></td>';
+html += '  </tr>';	
+html += '</tbody>';
 	
-	$('#product tfoot').before(html);
-		
-	$('#product-row' + product_row + ' .added_date').datepicker({dateFormat: 'yy-mm-dd'});
-	
-	product_row++;
+$('#product tfoot').before(html);
+product_row++;
 }
 
-$( document ).ready(function() {
-  $("input.added_date").datepicker();
-});
