8b5464d1a05fcff9d63b8fa25c46499f38684b48
Prem Bikram Limbu <moranprem@rocketmail.com>
2015-07-31 19:47:50 +0530

    Update addrow.js

--- addrow.js
+++ addrow.js
@@ -3,9 +3,7 @@ var product_row = 0;
 function addProduct() {
 	html  = '<tbody id="product-row' + product_row + '">';
 	html += '  <tr>'; 
-  html += '    <td class="left"><select name="product[' + product_row + '][product_name]">';
-  html += '      <option value="1">Default</option>';
-  html += '    </select></td>';		
+  html += '    <td class="left"><input type="text" name="product[' + product_row + '][name]" value=""/></td>';		
   html += '    <td class="right"><input type="text" name="product[' + product_row + '][quantity]" value="" size="2" /></td>';
   html += '    <td class="right"><input type="text" name="product[' + product_row + '][listing_priority]" value="" size="2" /></td>';
 	html += '    <td class="right"><input type="text" name="product[' + product_row + '][price]" value="" /></td>';
